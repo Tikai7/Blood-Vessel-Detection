14050fae91d44a343afec989e434bfe74690ee6a
Tikai7 <halimi.abdelkrim@outlook.fr>
2024-03-23 23:11:55 +0100

    added a notebook file to do some tests

--- src/model/Test.py
+++ src/model/Test.py
@@ -9,9 +9,9 @@ class Tester():
     """A class to represent the testing process for the U-Net model for vessel segmentation.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, state="params/local_model_EN_b3_BD_adamW_weight_augmented_3D_3C_100epochs") -> None:
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.state = torch.load("params/local_model_EN_b3_BD_adamW_weight_augmented_3D_3C_100epochs")
+        self.state = torch.load(state)
         encoder_name = 'efficientnet-b3'
         model = smp.Unet(encoder_name=encoder_name, classes=1, in_channels=3)
         model.load_state_dict(self.state)
@@ -55,31 +55,5 @@ class Tester():
         masked_image = image*mask
         mask_image_transposed = np.transpose(masked_image, (1, 2, 0))
         return mask, mask_image_transposed
-
-tester = Tester()
-
-patch = "2_1265_001B._patches_26_46.png"
-cluster = "001B_clustd"
-
-image = Image.open(f"patches/patches_bvd_clustd/{cluster}/{patch}")
-mask = Image.open(f"patches/patches_bvd_clustd_mask/{cluster}/{patch}").convert('L')
-
-mask_pred, mask_image = tester.predict(image.copy())
- 
-plt.figure(figsize=(10,10))
-plt.subplot(1,4,1)
-plt.imshow(image)
-plt.title("Original Image")
-plt.subplot(1,4,2)
-plt.imshow(mask, cmap='gray')
-plt.title("Original Mask")
-plt.subplot(1,4,3)
-plt.imshow(mask_pred[0], cmap='gray')
-plt.title("Predicted Mask")
-plt.subplot(1,4,4)
-plt.imshow(mask_image)
-plt.title("Masked Image")
-plt.show()
-
 
 
